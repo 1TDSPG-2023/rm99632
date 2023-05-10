aab6bd6b17d689bd5e153f2d1a53393fda681c85
GEdO23 <rm99632@fiap.com.br>
2023-05-09 22:59:42 -0300

    Update estilo de site e funções

--- exercicio-1/exercicio.js
+++ exercicio-1/exercicio.js
@@ -2,38 +2,54 @@
 
 // 1 –Crie um botão que, quando clicado, muda a cor do background do body da página exercicio-1.html.
 
-let i = 1;
+let isDefault = true;
 
 const dfltBgndColor = 'var(--default-bgnd-color)';
 const dfltFontColor = 'var(--default-font-color)';
 const dfltLiColor = 'var(--default-li-color)';
 const darkLiColor = 'var(--dark-li-color)';
+const dfltTitColor = 'var(--default-tit-color)';
+const darkTitColor = 'var(--dark-tit-color)';
 
-function changeBackgroundColor() {
+function changeToDefault() {
     const select_all_li = document.querySelectorAll('li');
-    switch (i) {
-        default:
-            document.body.style.backgroundColor = dfltBgndColor;
-            document.body.style.color = dfltFontColor;
-            select_all_li.forEach(li => {
-                li.style.color = dfltLiColor;
-            })
-            i++;
-            break;
+    const select_all_tit = document.querySelectorAll('h1')
+    document.body.style.backgroundColor = dfltBgndColor;
+    document.body.style.color = dfltFontColor;
+    select_all_li.forEach(li => {
+        li.style.color = dfltLiColor;
+    })
+    select_all_tit.forEach(tit => {
+        tit.style.color = dfltTitColor;
+        tit.style.transition = '5s';
+    })
+}
 
-        case 1:
-            document.body.style.backgroundColor = dfltFontColor;
-            document.body.style.color = dfltBgndColor;
-            select_all_li.forEach(li => {
-                li.style.color = darkLiColor;
-            })
-            i--;
-            break;
+function changeToDark() {
+    const select_all_li = document.querySelectorAll('li');
+    const select_all_tit = document.querySelectorAll('h1')
+    document.body.style.backgroundColor = dfltFontColor;
+    document.body.style.color = dfltBgndColor;
+    select_all_li.forEach(li => {
+        li.style.color = darkLiColor;
+    })
+    select_all_tit.forEach(tit => {
+        tit.style.color = darkTitColor;
+        tit.style.transition = '5s';
+    })
+}
+        
+function changeBackgroundColor() {
+    if(isDefault) {
+        changeToDefault();
+        isDefault = false;
+    } else {
+        changeToDark();
+        isDefault = true;
     }
     document.body.style.transition = "1s";
 }
-
-
+        
 /*2–Crie um formulário com um campo de entrada de texto e um botão "Enviar". 
 Quando o botão for clicado, o valor do campo de entrada deve ser adicionado como um novo item em uma lista na página.
 Obs:
