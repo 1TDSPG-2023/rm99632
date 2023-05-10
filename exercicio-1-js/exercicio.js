74a5962ca1fa3f857dc2d3e7769645f036ababc5
GEdO23 <rm99632@fiap.com.br>
2023-05-09 22:11:07 -0300

    Adcionado formulario da lista de itens.

--- exercicio-1-js/exercicio.js
+++ exercicio-1-js/exercicio.js
@@ -7,20 +7,51 @@ let i = 1;
 const dfltBgndColor = 'var(--default-bgnd-color)';
 const dfltFontColor = 'var(--default-font-color)';
 const dfltLiColor = 'var(--default-li-color)';
+const darkLiColor = 'var(--dark-li-color)';
 
 function changeBackgroundColor() {
+    const select_all_li = document.querySelectorAll('li');
     switch (i) {
-        case 1:
-            document.body.style.backgroundColor = dfltFontColor;
-            document.body.style.color = dfltBgndColor;
-            i--;
-            break;
-    
         default:
             document.body.style.backgroundColor = dfltBgndColor;
             document.body.style.color = dfltFontColor;
+            select_all_li.forEach(li => {
+                li.style.color = dfltLiColor;
+            })
             i++;
+            break;
+
+        case 1:
+            document.body.style.backgroundColor = dfltFontColor;
+            document.body.style.color = dfltBgndColor;
+            select_all_li.forEach(li => {
+                li.style.color = darkLiColor;
+            })
+            i--;
             break;
     }
     document.body.style.transition = "1s";
 }
+
+
+/*2–Crie um formulário com um campo de entrada de texto e um botão "Enviar". 
+Quando o botão for clicado, o valor do campo de entrada deve ser adicionado como um novo item em uma lista na página.
+Obs:
+Utilizeo métodoconstnewItem = document.createElement('li'),para gerar um novo elemento li.
+Utilize o método list.appendChild(newItem), para poder adicionar o novo item na lista ul. 
+*/
+
+function adicionar() {
+    var item = document.getElementById("item").value;
+    var list = document.getElementById("list");
+    
+    if (item !== '') { // Verifica se o campo não está vazio
+      const newItem = document.createElement('li'); // Cria um novo elemento "li"
+      newItem.textContent = item; // Define o conteúdo do novo elemento com o valor do campo de entrada de texto
+      list.appendChild(newItem); // Adiciona o novo elemento na lista
+  
+      document.getElementById("item").value = ''; // Limpa o campo de entrada de texto
+    }
+}
+
+
